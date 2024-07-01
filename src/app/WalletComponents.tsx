b20fbc32a12e2a07fcad1fe2c19f97b32cbf9b34
fren <brettlayer3@gmail.com>
2024-07-01 05:15:39 -0400

    Update WalletComponents.tsx

--- src/app/WalletComponents.tsx
+++ src/app/WalletComponents.tsx
@@ -20,18 +20,19 @@ import {
       <div className="flex justify-end">
         <Wallet>
           <ConnectWallet>
-            <Avatar className="" />
-            <Name />
+            {/* <Avatar className="" />
+            <Name /> */}
+            <Address className={color.foregroundMuted} />
           </ConnectWallet>
           <WalletDropdown>
-            <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
+            {/* <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
               <Avatar />
               <Name>
-                {/* <Badge /> */}
+                <Badge />
               </Name>
               <Address className={color.foregroundMuted} />
               <EthBalance />
-            </Identity>
+            </Identity> */}
             <WalletDropdownLink icon="wallet" href="https://wallet.coinbase.com">
               Go to Wallet Dashboard
             </WalletDropdownLink>
